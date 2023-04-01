e8647eadf83e16cf5711f52826ce0bf153038419
Pranjal <pranjaljpatil@gmail.com>
2023-03-31 19:14:21 -0600

    del

--- main.cpp
+++ main.cpp
@@ -11,12 +11,23 @@ int main(){
     uint64_t t= 4;
     for(uint64_t i=1;i<=100;i++)
     {    ob->insert_(i);}
-        ob->print();
-    
+    ob->print2();
     for(uint64_t i=1;i<=100;i++)
         cout<<" "<<i<<" "<<ob->query_(i)<<endl;
     for(uint64_t i=1;i<=30;i++)
         ob->delete_(i);
+            
+    for(uint64_t i=1;i<=27;i++)
+    {  
+        ob->insert_(i);
+        cout<<"insert="<<i<<endl;
+        //ob->print2();
+        //cout<<"q";
+    }
+    ob->insert_(30);
+    ob->delete_(44);
+    ob->insert_(29);
+   // ob->print2();
     for(uint64_t i=1;i<=100;i++)
         cout<<" "<<i<<" "<<ob->query_(i)<<endl;
     return 1;
